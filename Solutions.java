4334d9d72a04b49f71cab598344a89d81bbeafa0
PratibhaNimbolkar <112884107+PratibhaNimbolkar@users.noreply.github.com>
2022-10-02 19:38:25 +0200

    COMMIT

---
+++ Solutions.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.*;
+
+import static java.util.stream.Collectors.joining;
+
+
+public class Solutions {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i = 1 ; i<= 10 ; i++){
+            int result  = N * i;
+            System.out.println(N+ " x " +i+ " = " +result);
+        }
+        bufferedReader.close();
+    }
+}
+
