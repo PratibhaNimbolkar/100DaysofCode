726e4b7056d6a7a54339d1e9a1e2f4c2cb4ea827
PratibhaNimbolkar <112884107+PratibhaNimbolkar@users.noreply.github.com>
2022-10-05 20:19:32 +0200

    Create Outputformatting.java

---
+++ Day5(OutputFormatting)/Outputformatting.java
@@ -0,0 +1,25 @@
+/*Every line of input will contain a String followed by an integer.
+Each String will have a maximum of 10 alphabetic characters, and each integer will be in the inclusive range  from 0 to 999 .*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                 System.out.printf("%-15s" , s1);
+                 System.out.printf("%03d" , x);
+                 System.out.println();
+                                  
+    
+            }
+            System.out.println("================================");
+
+    }
+}
+
