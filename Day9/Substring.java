fbbacd9aff84b595fc72bb1bd78b90e7ad101fc4
PratibhaNimbolkar <112884107+PratibhaNimbolkar@users.noreply.github.com>
2022-10-09 12:38:22 +0200

    Create Substring.java

---
+++ Day9/Substring.java
@@ -0,0 +1,20 @@
+//print a substring consisting of all characters in the inclusive range from  start  to  end.
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
